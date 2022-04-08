c85809560d87d56266df438efd3d494f4ba96d4a
bheema-284 <bheemudug@gmail.com>
2022-04-09 00:04:17 +0530

    some UI is added

--- form/src/pages/LoginOne.jsx
+++ form/src/pages/LoginOne.jsx
@@ -1,38 +1,60 @@
 import { useNavigate } from 'react-router-dom';
-import{useContext} from "react"
-import {LoginContext} from "../context/LoginContext"
+import '../App.css';
+import { useContext } from 'react';
+import { LoginContext } from '../context/LoginContext';
 export const LoginOne = () => {
-  const { name,age,dateofbirth, dispatch } = useContext(LoginContext);
+  const { name, age, dateofbirth, dispatch } = useContext(LoginContext);
   const navigate = useNavigate();
-  console.log(name,age,dateofbirth, dispatch);
+  console.log(name, age, dateofbirth, dispatch);
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter Name"
-        value={name}
-        onChange={(e) =>
-          dispatch({ type: 'CHANGE_NAME', payload: e.target.value })
-        }
-      />
-      <input
-        type="number"
-        placeholder="Enter Age"
-        value={age}
-        onChange={(e) =>
-          dispatch({ type: 'CHANGE_AGE', payload: e.target.value })
-        }
-      />
-      <input
-        type="date"
-        placeholder="Enter DateofBirth"
-        value={dateofbirth}
-        onChange={(e) =>
-          dispatch({ type: 'CHANGE_DATEOFBIRTH', payload: e.target.value })
-        }
-      />
-      <button disabled={!name && !age && !dateofbirth} onClick={() => navigate('/registration/two')}>next</button>
+    <div id="formdata">
+      <div id="da">
+        <div>
+          <label htmlFor="">Name:</label>
+          <br />
+          <label htmlFor=""> Age:</label>
+          <br />
+          <label htmlFor="">DateOfBirth:</label>
+          <br />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            value={name}
+            onChange={(e) =>
+              dispatch({ type: 'CHANGE_NAME', payload: e.target.value })
+            }
+          />
+          <br />
+          <input
+            type="number"
+            placeholder="Enter Age"
+            value={age}
+            onChange={(e) =>
+              dispatch({ type: 'CHANGE_AGE', payload: e.target.value })
+            }
+          />
+          <br />
+
+          <input
+            type="date"
+            placeholder="Enter DateofBirth"
+            value={dateofbirth}
+            onChange={(e) =>
+              dispatch({ type: 'CHANGE_DATEOFBIRTH', payload: e.target.value })
+            }
+          />
+          <br />
+          <button
+            style={{ width: '60px', backgroundColor: '#F6FFA4' }}
+            disabled={!name && !age && !dateofbirth}
+            onClick={() => navigate('/registration/two')}>
+            next
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- form/src/pages/LoginTwo.jsx
+++ form/src/pages/LoginTwo.jsx
@@ -17,36 +17,57 @@ export const LoginTwo = () => {
         pincode, dispatch);
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter StateofResidence"
-        value={stateofresidence}
-        onChange={(e) =>
-          dispatch({ type: 'CHANGE_STATEOFESIDENCE', payload: e.target.value })
-        }
-      />
-      <input
-        type="text"
-        placeholder="Enter Address"
-        value={address}
-        onChange={(e) =>
-          dispatch({ type: 'CHANGE_ADDRESS', payload: e.target.value })
-        }
-      />
-      <input
-        type="text"
-        placeholder="Enter Pincode"
-        value={pincode}
-        onChange={(e) =>
-          dispatch({ type: 'CHANGE_PINCODE', payload: e.target.value })
-        }
-      />
-      <button
-        disabled={!stateofresidence || !address || !pincode}
-        onClick={()=>{handleSubmit()}}>
-        submit
-      </button>
+    <div id="formdata">
+      <div id="da">
+        <div>
+          <label htmlFor="">State:</label>
+          <br />
+          <label htmlFor=""> Address:</label>
+          <br />
+          <label htmlFor="">Pincode:</label>
+          <br />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter StateofResidence"
+            value={stateofresidence}
+            onChange={(e) =>
+              dispatch({
+                type: 'CHANGE_STATEOFESIDENCE',
+                payload: e.target.value,
+              })
+            }
+          />{' '}
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Address"
+            value={address}
+            onChange={(e) =>
+              dispatch({ type: 'CHANGE_ADDRESS', payload: e.target.value })
+            }
+          />{' '}
+          <br />
+          <input
+            type="text"
+            placeholder="Enter Pincode"
+            value={pincode}
+            onChange={(e) =>
+              dispatch({ type: 'CHANGE_PINCODE', payload: e.target.value })
+            }
+          />
+          <br />
+          <button
+            style={{ width: '60px', backgroundColor: '#FFD93D' }}
+            disabled={!stateofresidence || !address || !pincode}
+            onClick={() => {
+              handleSubmit();
+            }}>
+            submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
